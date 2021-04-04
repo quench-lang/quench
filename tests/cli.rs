fcb2dfbedd23d108e9342455f205f00925e1f9ee
Sam Estep <sam@samestep.com>
2021-04-03 17:37:15 -0700

    Run compiled JavaScript programs using Deno

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::{assert::OutputAssertExt, prelude::CommandCargoExt};
 use goldenfile::Mint;
 use std::{
     ffi::OsStr,
+    path::Path,
     process::{Command, Output},
 };
 use std::{fs, io::Write};
@@ -16,37 +17,41 @@ fn test_help() {
     file.write_all(&assert.get_output().stdout).unwrap();
 }
 
+fn assert_expected(subcmd: &str, src: &Path) {
+    let mut cmd = Command::cargo_bin("quench").unwrap();
+    let assert = cmd.env("NO_COLOR", "1").arg(subcmd).arg(&src).assert();
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = assert.get_output();
+
+    if stderr.is_empty() {
+        assert!(status.success());
+    } else {
+        assert!(!status.success());
+    }
+
+    let dir = src.parent().unwrap().join(subcmd);
+    let txt_path = src.with_extension("txt");
+    let txt_name = Path::new(txt_path.file_name().unwrap());
+
+    let mut out_mint = Mint::new(dir.join("out"));
+    let mut out = out_mint.new_goldenfile(txt_name).unwrap();
+    out.write_all(stdout).unwrap();
+
+    let mut err_mint = Mint::new(dir.join("err"));
+    let mut err = err_mint.new_goldenfile(txt_name).unwrap();
+    err.write_all(stderr).unwrap();
+}
+
 #[test]
 fn test_examples() {
-    let dir = "examples";
-    let mut mint = Mint::new(dir);
-
-    for entry in fs::read_dir(dir).unwrap() {
+    for entry in fs::read_dir("examples").unwrap() {
         let path = entry.unwrap().path();
         if path.extension() == Some(OsStr::new("qn")) {
-            let mut cmd = Command::cargo_bin("quench").unwrap();
-            let assert = cmd.arg("run").arg(&path).assert();
-            let Output {
-                status,
-                stdout,
-                stderr,
-            } = assert.get_output();
-
-            if stderr.is_empty() {
-                assert!(status.success());
-            } else {
-                assert!(!status.success());
-            }
-
-            let mut out = mint
-                .new_goldenfile(path.with_extension("out.txt").file_name().unwrap())
-                .unwrap();
-            out.write_all(stdout).unwrap();
-
-            let mut err = mint
-                .new_goldenfile(path.with_extension("err.txt").file_name().unwrap())
-                .unwrap();
-            err.write_all(stderr).unwrap();
+            assert_expected("compile", &path);
+            assert_expected("run", &path);
         }
     }
 }
