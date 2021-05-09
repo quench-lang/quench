ab60f67d9e0fa51313f585caf9b99c01f836f454
Sam Estep <sam@samestep.com>
2021-05-09 00:06:36 -0700

    Add some more syntax (#62)

--- src/db.rs
+++ src/db.rs
@@ -26,15 +26,23 @@ impl Eq for Cst {}
 
 #[derive(Clone, Copy, EnumIter)]
 enum TokenType {
+    Boolean,
     Comment,
+    Integer,
+    Null,
     String,
+    Symbol,
     Variable,
 }
 
 fn semantic_token_type(token_type: TokenType) -> SemanticTokenType {
     match token_type {
+        TokenType::Boolean => SemanticTokenType::ENUM_MEMBER,
         TokenType::Comment => SemanticTokenType::COMMENT,
+        TokenType::Integer => SemanticTokenType::NUMBER,
+        TokenType::Null => SemanticTokenType::ENUM_MEMBER,
         TokenType::String => SemanticTokenType::STRING,
+        TokenType::Symbol => SemanticTokenType::PROPERTY,
         TokenType::Variable => SemanticTokenType::VARIABLE,
     }
 }
@@ -285,8 +293,12 @@ impl AbsoluteToken {
 
 fn absolute_tokens(node: &Node, index: &text::Index) -> im::Vector<AbsoluteToken> {
     if let Some(token_type) = match node.kind() {
+        "boolean" => Some(TokenType::Boolean),
         "comment" => Some(TokenType::Comment),
+        "integer" => Some(TokenType::Integer),
+        "null" => Some(TokenType::Null),
         "string" => Some(TokenType::String),
+        "symbol" => Some(TokenType::Symbol),
         "identifier" => Some(TokenType::Variable),
         _ => None,
     } {
@@ -537,8 +549,8 @@ mod tests {
         assert_eq!(
             diagnostics,
             im::vector![
-                make_error(6, (0, 6), 14, (0, 14), "syntax (ERROR (string))"),
-                make_error(24, (0, 24), 24, (0, 24), "syntax (MISSING \";\")"),
+                make_error(10, (0, 10), 12, (0, 12), "syntax (ERROR)"),
+                make_error(34, (0, 34), 34, (0, 34), "syntax (MISSING \";\")"),
             ],
         );
     }
@@ -566,7 +578,9 @@ mod tests {
             tokens,
             im::vector![
                 make_token(0, 0, 21, TokenType::Comment),
-                make_token(2, 0, 5, TokenType::Variable),
+                make_token(1, 0, 4, TokenType::Variable),
+                make_token(0, 8, 1, TokenType::Variable),
+                make_token(0, 5, 5, TokenType::Variable),
                 make_token(0, 6, 15, TokenType::String),
             ],
         );
@@ -594,19 +608,30 @@ mod tests {
         let (db, uri) = foo_db(slurp::read_all_to_string("examples/hello.qn").unwrap());
         let tree = db.ast(uri).unwrap();
         let expected = syntax::File {
-            range: ts_range(0, (0, 0), 47, (3, 0)),
-            body: vec![syntax::Stmt {
-                range: ts_range(23, (2, 0), 46, (2, 23)),
-                expression: syntax::Expr::Call(syntax::Call {
-                    range: ts_range(23, (2, 0), 45, (2, 22)),
-                    function: syntax::Id {
-                        range: ts_range(23, (2, 0), 28, (2, 5)),
-                        name: String::from("print"),
+            range: ts_range(0, (0, 0), 58, (2, 0)),
+            decls: vec![syntax::Decl {
+                range: ts_range(22, (1, 0), 57, (1, 35)),
+                name: syntax::Id {
+                    range: ts_range(22, (1, 0), 26, (1, 4)),
+                    name: String::from("main"),
+                },
+                val: syntax::Expr::Func(syntax::Func {
+                    range: ts_range(30, (1, 8), 56, (1, 34)),
+                    param: syntax::Id {
+                        range: ts_range(30, (1, 8), 31, (1, 9)),
+                        name: String::from("_"),
                     },
-                    arguments: vec![syntax::Expr::Lit(syntax::Lit::Str(syntax::Str {
-                        range: ts_range(29, (2, 6), 44, (2, 21)),
-                        value: String::from("Hello, world!"),
-                    }))],
+                    body: Box::new(syntax::Expr::Call(syntax::Call {
+                        range: ts_range(35, (1, 13), 56, (1, 34)),
+                        func: Box::new(syntax::Expr::Id(syntax::Id {
+                            range: ts_range(35, (1, 13), 40, (1, 18)),
+                            name: String::from("print"),
+                        })),
+                        arg: Box::new(syntax::Expr::Lit(syntax::Lit::Str(syntax::Str {
+                            range: ts_range(41, (1, 19), 56, (1, 34)),
+                            val: String::from("Hello, world!"),
+                        }))),
+                    })),
                 }),
             }],
         };
@@ -623,29 +648,68 @@ mod tests {
                 "type": "Program",
                 "body": [
                     {
+                        "type": "VariableDeclaration",
+                        "declarations": [
+                            {
+                                "type": "VariableDeclarator",
+                                "id": {
+                                    "type": "Identifier",
+                                    "name": "main"
+                                },
+                                "init": {
+                                    "type": "FunctionExpression",
+                                    "id": null,
+                                    "params": [
+                                        {
+                                            "type": "Identifier",
+                                            "name": "_"
+                                        }
+                                    ],
+                                    "body": {
+                                        "type": "BlockStatement",
+                                        "body": [
+                                            {
+                                                "type": "ReturnStatement",
+                                                "argument": {
+                                                    "type": "CallExpression",
+                                                    "callee": {
+                                                        "type": "MemberExpression",
+                                                        "object": {
+                                                            "type": "Identifier",
+                                                            "name": "console"
+                                                        },
+                                                        "property": {
+                                                            "type": "Identifier",
+                                                            "name": "log"
+                                                        },
+                                                        "computed": false,
+                                                    },
+                                                    "arguments": [
+                                                        {
+                                                            "type": "Literal",
+                                                            "value": "Hello, world!",
+                                                        }
+                                                    ],
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        ],
+                        "kind": "var"
+                    },
+                    {
                         "type": "ExpressionStatement",
                         "expression": {
                             "type": "CallExpression",
                             "callee": {
-                                "type": "MemberExpression",
-                                "object": {
-                                    "type": "Identifier",
-                                    "name": "console",
-                                },
-                                "property": {
-                                    "type": "Identifier",
-                                    "name": "log",
-                                },
-                                "computed": false,
+                                "type": "Identifier",
+                                "name": "main"
                             },
-                            "arguments": [
-                                {
-                                    "type": "Literal",
-                                    "value": "Hello, world!",
-                                },
-                            ],
-                        },
-                    },
+                            "arguments": [],
+                        }
+                    }
                 ],
             }),
         );
